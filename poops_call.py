b20cdc1f51dc95ac53be271cd0867b01e268daed
JaeSeo-akkung <jssd0328@gmail.com>
2025-11-02 16:00:20 +0900

    재서 파이썬 1번 프로젝트

---
+++ poops_call.py
@@ -0,0 +1,10 @@
+import poops
+
+
+z = poops.add(8,3)
+x = poops.add(5,3)
+y = poops.add(1,2)
+
+print("poops_call 함수 호출한 결과 찍기",z)
+print("poops_call 함수 호출한 결과 찍기",x)
+print("poops_call 함수 호출한 결과 찍기",y)
